0cedfee9ba9b9cbd52e7e7130d8eade672df3a40
Yuha <regen@gmail.com>
2026-01-15 19:48:41 +0900

    python_기본자료형

---
+++ practice.py
@@ -0,0 +1,32 @@
+print('hello world');
+
+# 숫자형 자료형!
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+8)
+print(2*8)
+print(3*(3+1))
+
+# 정수형
+a = 1
+print(a, type(a))
+# 실수형
+b = 1.0
+print(b, type(b))
+
+# 문자열 자료형
+print('풍선')
+print("바다")
+print('꽃' + '구름')
+print('zz')
+print('z'*9)
+
+# boolean 자료형
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
